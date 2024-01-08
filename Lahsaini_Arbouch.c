c2a6c7c674373eee199b96fcf84440a65f135e87
lahsainiyassine <155819883+lahsainiyassine@users.noreply.github.com>
2024-01-08 20:41:53 +0100

    Update and rename MiniProjet (2).c to Lahsaini_Arbouch.c

--- Lahsaini_Arbouch.c
+++ Lahsaini_Arbouch.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-// Structure pour représenter une question
+// Structure pour reprÃ©senter une question
 struct Question {
     char enonce[100];
     char choix1[50];
@@ -10,10 +9,10 @@ struct Question {
     char choix3[50];
     char choix4[50];
     char reponseCorrecte;
-    int difficulte; // Ajout de la difficulté (1 pour facile, 2 pour moyen, 3 pour difficile)
+    int difficulte; // Ajout de la difficultÃ© (1 pour facile, 2 pour moyen, 3 pour difficile)
 };
 
-// Fonction pour poser une question et obtenir la réponse du joueur
+// Fonction pour poser une question et obtenir la rÃ©ponse du joueur
 char poserQuestion(struct Question *question) {
     printf("%s\n", question->enonce);
     printf("a) %s\n", question->choix1);
@@ -22,25 +21,25 @@ char poserQuestion(struct Question *question) {
     printf("d) %s\n", question->choix4);
 
     char reponse;
-    printf("Votre réponse (a, b, c, d) : ");
+    printf("Votre rÃ©ponse (a, b, c, d) : ");
     scanf(" %c", &reponse);
 
     return reponse;
 }
 
-// Fonction pour vérifier la réponse du joueur
+// Fonction pour vÃ©rifier la rÃ©ponse du joueur
 int verifierReponse(char reponseJoueur, struct Question *question, int *score, int *vie, int *pointsGagnes) {
     if (reponseJoueur == question->reponseCorrecte) {
-        printf("Bonne réponse !\n");
+        printf("Bonne rÃ©ponse !\n");
         int points = (question->difficulte == 3) ? 10 : 6; // Plus de points pour les questions difficiles
         (*score) += points;
         (*pointsGagnes) += points;
-        return 1; // Retourne 1 si la réponse est correcte
+        return 1; // Retourne 1 si la rÃ©ponse est correcte
     } else {
-        printf("Mauvaise réponse. Vous perdez une vie. La bonne réponse était : %c\n", question->reponseCorrecte);
+        printf("Mauvaise rÃ©ponse. Vous perdez une vie. La bonne rÃ©ponse Ã©tait : %c\n", question->reponseCorrecte);
         (*vie) -= 1;
         printf("Il vous reste %d vies.\n", *vie);
-        return 0; // Retourne 0 si la réponse est incorrecte
+        return 0; // Retourne 0 si la rÃ©ponse est incorrecte
     }
 }
 
@@ -49,31 +48,31 @@ int main() {
     // Initialisation des questions
     struct Question questions[16] = {
         {"Quel est le plus petit pays du monde ?", "Vatican", "Nauru", "Saint-Marin", "Maldives", 'a', 1},
-        {"Quel pays a remporté la Coupe du Monde de football en 2014 ?", "Argentine", "Brésil", "Allemagne", "Italie", 'c', 2},
+        {"Quel pays a remportÃ© la Coupe du Monde de football en 2014 ?", "Argentine", "BrÃ©sil", "Allemagne", "Italie", 'c', 2},
         {"Qui est actuellement le roi du Maroc ?", "Hassan 2", "Le Pacha de Marrakech", "Mohammed Ali", "Mohammed 6", 'd', 1},
-        {"Quel est l'élément le plus abondant dans l'atmosphère terrestre ?", "Azote", "Oxygène", "Dioxyde de carbone", "Hydrogène", 'a', 3},
-        {"Quel est l'animal national de l'Australie ?", "Kangourou", "Koala", "Émeu", "Dingo", 'a', 3},//
-        {"Qui a écrit Le Petit Prince ?", "Victor Hugo", "Antoine de Saint-Exupéry", "Albert Camus", "Marcel Proust", 'b', 2},
-        {"Quel est le plus grand océan du monde ?", "Guépard", "Lion", "Antilope", "Zèbre", 'a', 3},
-        {"Quel est l'organe le plus grand du corps humain ?", "Cerveau", "Peau", "Foie", "Cœur", 'a', 3},
+        {"Quel est l'Ã©lÃ©ment le plus abondant dans l'atmosphÃ¨re terrestre ?", "Azote", "OxygÃ¨ne", "Dioxyde de carbone", "HydrogÃ¨ne", 'a', 3},
+        {"Quel est l'animal national de l'Australie ?", "Kangourou", "Koala", "Ã‰meu", "Dingo", 'a', 3},//
+        {"Qui a Ã©crit Le Petit Prince ?", "Victor Hugo", "Antoine de Saint-ExupÃ©ry", "Albert Camus", "Marcel Proust", 'b', 2},
+        {"Quel est le plus grand ocÃ©an du monde ?", "GuÃ©pard", "Lion", "Antilope", "ZÃ¨bre", 'a', 3},
+        {"Quel est l'organe le plus grand du corps humain ?", "Cerveau", "Peau", "Foie", "CÅ“ur", 'a', 3},
         {"Quel est le plus haut sommet du monde ?", "Mont Everest", "Mont Kilimandjaro", "Mont McKinley (Denali)", "Mont Fuji", 'a', 3},
-        {"Quelle est la planète la plus proche du soleil ?", "Vénus", "Terre", "Mars", "Mercure", 'd', 3},
+        {"Quelle est la planÃ¨te la plus proche du soleil ?", "VÃ©nus", "Terre", "Mars", "Mercure", 'd', 3},
         {"Dans quelle ville se trouve la tour Eiffel ?", "monaco", "lille", "roma", "paris", 'd', 1},
         {"Quelle est la capitale de l'Espagne ?", "valence", "madrid", "barcalona", "seville", 'b', 1},
         {"quel pays gagnera la coupe du monde 2022 ?", "france", "brazil", "maroc", "argentine", 'd', 1},
-        {"Qui a remporté le Ballon d'Or 2021, récompensant le meilleur joueur de football au monde ?", "neymar", "cristiano ronaldo", "lionel messi", "xavi", 'c', 2},
-        {"Dans quel sport Michael Phelps a-t-il remporté le plus grand nombre de médailles olympiques?", "Athlétisme ", "Natation ", "Gymnastique ", "Tennis ", 'b', 2},
-        {"Quel joueur de tennis détient le record du plus grand nombre de victoires en Grand Chelem en simple chez les hommes ?", "Rafael Nadal", "Novak Djokovic","Roger Federer", "Pete Sampras ", 'b', 2},
+        {"Qui a remportÃ© le Ballon d'Or 2021, rÃ©compensant le meilleur joueur de football au monde ?", "neymar", "cristiano ronaldo", "lionel messi", "xavi", 'c', 2},
+        {"Dans quel sport Michael Phelps a-t-il remportÃ© le plus grand nombre de mÃ©dailles olympiques?", "AthlÃ©tisme ", "Natation ", "Gymnastique ", "Tennis ", 'b', 2},
+        {"Quel joueur de tennis dÃ©tient le record du plus grand nombre de victoires en Grand Chelem en simple chez les hommes ?", "Rafael Nadal", "Novak Djokovic","Roger Federer", "Pete Sampras ", 'b', 2},
     };
 
     int score = 0;
     int vie = 3;
-    int pointsGagnes = 0; // Variable pour suivre le total des points gagnés
+    int pointsGagnes = 0; // Variable pour suivre le total des points gagnÃ©s
 
-    // Choix du niveau de difficulté
+    // Choix du niveau de difficultÃ©
     int difficulte;
     printf("\t\tBonjour! J'espere que vous etes pris pour un test de questions pour un champion  :\n");
-    printf("Choisissez le niveau de difficulté(1 pour facile, 2 pour moyen, 3 pour difficile) : ");
+    printf("Choisissez le niveau de difficultÃ©(1 pour facile, 2 pour moyen, 3 pour difficile) : ");
     scanf("%d", &difficulte);
 
     // Boucle de jeu
@@ -84,15 +83,15 @@ int main() {
         }
     }
 
-    // Afficher le résultat du jeu
+    // Afficher le rÃ©sultat du jeu
     if (score >= 100 && vie > 0) {
-        printf("Félicitations, vous avez gagné !\n");
+        printf("FÃ©licitations, vous avez gagnÃ© !\n");
     } else {
         printf("Game over. Vous n'avez plus de vies ou n'avez pas atteint le score requis.\n");
     }
 
-    // Afficher le total des points gagnés
-    printf("Total des points gagnés : %d\n", pointsGagnes);
+    // Afficher le total des points gagnÃ©s
+    printf("Total des points gagnÃ©s : %d\n", pointsGagnes);
 
     return 0;
 }
